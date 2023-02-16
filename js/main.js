46a4a426763cc9d6599c19996d6480c2fc813dbc
luiscondori98 <lcondori11@hotmail.com>
2023-02-15 19:09:14 -0500

    agregando inputs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const producto = document.getElementById("container-productos")
+const containerProducto = document.getElementById("container-productos")
+const containerProductoFiltrados = document.getElementById("container-productos2")
 const iconoCarrito = document.getElementById("icarrito")
 
 fetch("/js/productos.json")
     .then((respo) => respo.json())
     .then((data) => {
 
-        const agregarCardProducto = product => {
+        agregarCardProducto = product => {
             const divProd = document.createElement("div")
             divProd.className = "item-producto"
             divProd.innerHTML = `
@@ -17,7 +18,9 @@ fetch("/js/productos.json")
             </div>
             `
 
-            producto.append(divProd);
+            containerProducto.append(divProd);
+
+            buscador(product);
         }
         
         for (const product of data) {
@@ -27,13 +30,47 @@ fetch("/js/productos.json")
         botonAgregar();
     })
 
+
+const buscador = (producto) => {
+    const buscador = document.getElementById("input-buscar")
+
+    buscador.addEventListener("focus", () => {
+        buscador.addEventListener("input", () => {
+
+            if(buscador.value === producto.nombre || buscador.value === producto.marca) {
+                console.log(producto)
+                containerProducto.remove()
+                botonAgregar()
+                setTimeout(() => {
+                    const divProd = document.createElement("div")
+                    divProd.className = "item-producto"
+                    divProd.innerHTML = `
+                    <img src="${producto.img}">
+                    <div class="descripcion">
+                        <h4>${producto.nombre}</h4>
+                        <h5>${producto.precio}</h5>
+                        <button type="submit" class="btn-agregar">Agregar</button>
+                    </div>
+                    `
+                containerProductoFiltrados.append(divProd)
+                botonAgregar();
+                }, "1000")
+            }
+
+            if(buscador.value === "") {
+                containerProductoFiltrados.remove();
+            }
+        })
+    })
+}
+
 const botonAgregar = () => {
     const botonAgregarCarrito = document.getElementsByClassName("btn-agregar")
 
     for(const buttons of botonAgregarCarrito){
 
         buttons.addEventListener("click", () => {
-
+        
         iconoCarrito.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-check-fill" viewBox="0 0 16 16">
         <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708z"/>
@@ -51,7 +88,6 @@ const botonAgregar = () => {
             },
             }).showToast();
         })
-
     }
 }
 
@@ -60,7 +96,7 @@ const botonNewslatter = document.getElementById("btn-newslatter").addEventListen
         text: "Enviado",
         gravity: "bottom",
         style: {
-            background: "red",
+            background: "rgb(177, 0, 0)",
             "border-radius" : "10px"
         },
       }).showToast();
@@ -72,10 +108,11 @@ const filtro = document.getElementById("ifilter")
 filtro.addEventListener("focus", () => {
     const select = document.createElement("ul")
     select.className = "select-filtro"
+    select.id = "select-filter"
     select.innerHTML = `
-    <li>Mayor precio</li>
-    <li>Menor precio</li>
-    <li>Destacados</li>
+    <li value="mayor">Mayor precio</li>
+    <li value="menor">Menor precio</li>
+    <li value="destacados">Destacados</li>
     `
     containerFiltro.append(select);
 
